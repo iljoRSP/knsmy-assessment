70ed55ceb321b61f009040eee46bfd6faa718eb6
iljoRSP <51939539+iljoRSP@users.noreply.github.com>
2022-06-23 20:11:33 +0800

    Delete 5 prune_repeats directory

--- 5 prune_repeats/pruneRepeats.js
+++
@@ -1,8 +0,0 @@
-var pruneRepeats = (word) => {
-	return word
-		.split('')  // split into array of chars
-        .filter((item, pos, w) => {         // keep only characters that
-            return w.indexOf(item) == pos;  // have their position as the pos of the first instance of that char
-        })                                  // effectively, any occurences after the first (dupes) will be filtered out
-        .join('');  // rejoin into string
-}
