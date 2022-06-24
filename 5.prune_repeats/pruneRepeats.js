1151e5c13b8c0475f10f0f051845cb84bc26a215
iljoRSP <51939539+iljoRSP@users.noreply.github.com>
2022-06-24 16:07:00 +0800

    Update pruneRepeats.js

--- 5.prune_repeats/pruneRepeats.js
+++ 5.prune_repeats/pruneRepeats.js
@@ -1,4 +1,4 @@
-var pruneRepeats = (word) => {
+const pruneRepeats = (word) => {
 	return word
 		.split('')  // split into array of chars
         .filter((item, pos, w) => {         // keep only characters that
@@ -6,3 +6,6 @@ var pruneRepeats = (word) => {
         })                                  // effectively, any occurences after the first (dupes) will be filtered out
         .join('');  // rejoin into string
 }
+
+
+console.log(pruneRepeats('elephant toothpaste'));  // outputs elphant os
